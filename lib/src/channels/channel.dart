74d59e6a9bd3168e8a02143aacb6ffd456faabbc
Jesus Acosta <jesusgabriel.91@gmail.com>
2020-11-26 17:52:36 -0400

    Models: Channel: add toMap and fromMap functions

--- lib/src/channels/channel.dart
+++ lib/src/channels/channel.dart
@@ -19,6 +19,24 @@ class Channel with EquatableMixin {
   /// Initializes an instance of [Channel]
   Channel(this.id, this.title, this.logoUrl);
 
+  /// Transform Channel instance to Map
+  Map<String, dynamic> toMap(Channel channel) {
+    return {
+      'id': id,
+      'title': title,
+      'logoUrl': logoUrl
+    };
+  }
+
+  /// Get a Channel instance from Map
+  Channel fromMap(Map<String, dynamic> map) {
+    return Channel(
+      map['id'],
+      map['title'],
+      map['logoUrl']
+    );
+  }
+
   @override
   String toString() => 'Channel ($title)';
 
